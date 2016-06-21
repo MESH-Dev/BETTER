43f33e1bc09ba3ce57034a3a29a784360e0f33e3
Shaun <shaun@meshfresh.com>
2016-06-21 18:12:05 -0400

    Initial Commit

--- js/mesh.js
+++ js/mesh.js
@@ -5,6 +5,56 @@ jQuery(document).ready(function($){
 
   //Let's do something awesome!
 
+  //Smooth page scroll
+  $(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          //'top-50' is custom.  limits the offset to top of window plus 50px
+          scrollTop: (target.offset().top-50)
+        }, 800);
+        return false;
+      }
+    }
+  });
+});
+
+   //external link manager
+
+  // New tabs
+
+  /* ==========
+     Variables
+   ========== */
+   var url = location.protocol+'//'+location.hostname+(location.port ? ':'+location.port: '');
+
+  /* ==========
+      Utilities
+    ========== */
+   function beginsWith(needle, haystack){
+     return (haystack.substr(0, needle.length) == needle);
+   };
+
+
+  /* ==========
+     Anchors open in new tab/window
+   ========== */
+   $('a').each(function(){
+
+     if(typeof $(this).attr('href') != "undefined") {
+      var test = beginsWith( url, $(this).attr('href') );
+      //if it's an external link then open in a new tab
+      if( test == false && $(this).attr('href').indexOf('#') == -1){
+        $(this).attr('target','_blank').prepend('<span class="sr-only">External link, opens in new window</span>');
+      }
+     }
+   });
+
+   //=============================================================
+
   //Sidr
 
 $('.sidr-trigger').sidr({
