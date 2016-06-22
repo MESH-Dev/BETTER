851d1253edfcf5835c5fa2e5edf1551de98d966e
Shaun <shaun@meshfresh.com>
2016-06-22 09:47:24 -0400

    Added Bugherd script

--- header.php
+++ header.php
@@ -31,6 +31,16 @@
 
 	<?php wp_head(); ?>
 
+	<!-- Bugherd -->
+	<script type='text/javascript'>
+		(function (d, t) {
+		  var bh = d.createElement(t), s = d.getElementsByTagName(t)[0];
+		  bh.type = 'text/javascript';
+		  bh.src = 'https://www.bugherd.com/sidebarv2.js?apikey=xu7uecwbwdpxjwvmzbtjgg';
+		  s.parentNode.insertBefore(bh, s);
+			  })(document, 'script');
+	</script>
+
 </head>
 
 <body <?php body_class(); ?>>
