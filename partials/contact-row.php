18ecb2cf963e4e915a4e67be7ff0e53f41089fdf
Shaun <shaun@meshfresh.com>
2016-07-27 16:41:58 -0400

    Changes per 20160727 QA

--- partials/contact-row.php
+++ partials/contact-row.php
@@ -2,7 +2,7 @@
       <div class="row">
         <p>
             <?php echo get_field('contact_text_line_1', 'options') ?></br>
-            <a href="mailto:<?php echo get_field('link_text', 'options'); ?>" ><?php echo get_field('contact_text_line_2', 'options') ?></a> <!--<img src="<?php echo get_template_directory_uri(); ?>/img/right-arrow.png">-->
+            <a href="<?php echo get_field('link_text', 'options'); ?>" ><?php echo get_field('contact_text_line_2', 'options') ?></a> <!--<img src="<?php echo get_template_directory_uri(); ?>/img/right-arrow.png">-->
 
           </p>
       </div>
