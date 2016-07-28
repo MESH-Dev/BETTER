f735306628f3a9f6e9dd16c8a07e285a430ad848
Shaun <shaun@meshfresh.com>
2016-07-28 14:52:47 -0400

    Changes per 20160727 QA

--- 404.php
+++ 404.php
@@ -3,12 +3,12 @@
 <div id="content">
 	
 	<h1>Page Not Found</h1>
-	<p>The page you requested could not be found. Perhaps searching will help.</p>
+	<p>The page you requested could not be found. </p>
 	
-	<?php get_search_form(); ?>
+	<?php //get_search_form(); ?>
 
 </div><!-- End of Content -->
 
 
-<?php get_sidebar(); ?>
+<?php //get_sidebar(); ?>
 <?php get_footer(); ?>
